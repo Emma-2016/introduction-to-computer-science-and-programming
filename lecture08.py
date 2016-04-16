f6ced519f8280ab4c89dd315ecc3819e448efd62
Emma-2016 <jiangchongyi2016@gmail.com>
2016-04-16 12:50:50 +0800

    Create lecture08.py

---
+++ lecture08.py
@@ -0,0 +1,5 @@
+#the rate of growth as size of problem grows
+#Asymptotic notation - Big O notation - upper limit to growth of function as input gets large
+f(x) O(n^2) n measures the size of x
+
+#recursion exponent
