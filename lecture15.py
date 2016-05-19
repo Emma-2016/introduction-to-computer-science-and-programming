ceec22bcd8245f4d83e3da3f4a3d74eae5d3129a
Jiang Chongyi <jiangchongyi2016@gmail.com>
2016-05-19 20:20:31 +0800

    Create lecture15.py

---
+++ lecture15.py
@@ -0,0 +1,47 @@
+Class - template to create instance of object.
+Instance has some internal attributes.
+
+class cartesianPoint:
+  pass
+  
+cp1 = cartesianPoint()
+cp2 = cartesianPoint()
+cp1.x = 1.0
+cp2.x = 1.0
+cp1.y = 2.0
+cp2.y = 3.0
+
+def samePoint(p1, p2):
+  return (p1.x == p2.x) and (p1.y == p2.y)
+  
+def printPoint(p):
+  print '(' + str(p.x) + ', ' + str(p.y) + ')'
+  
+  
+shallow equality -- 'is': given two things, do they point to exactly the same reference? (object)
+deep equality -- we can define. (value)
+
+
+class cPoint():
+  #def __init__(self, x, y): create an instance, use 'self' to refer to that instance
+  #when call a class, it create an instance with all those internal attributes. __init__ create a pointer to that instance.
+  #You need access to that, so it calls it, passing in self as the pointer to the instance.
+  #That says, it has access to that piece of memory and now inside of that piece of memory, I can do thing like:
+  #define self.x to be the value passed in for x. That is create a variable name x and a value associated with it.
+  #self will always point to a particular instance
+  
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+    self.radius = math.sqrt(self.x * self.x + self.y * self.y)
+    self. angle = math.atan2(self.y, self.x)
+  def cartesian(self):
+    return (self.x, self.y)
+  def polar(self):
+    return (self.radius, self.angle)
+  def __str__(self):
+    return '(' + str(self.x) + ', ' + str(self.y) + ')'
+  def __cmp__(self, other):
+    reutn (self.x > other.x) and (self.y > other.y)
+    
+#data hidding - one can only access instance values through defined methods. Python does not do this.
